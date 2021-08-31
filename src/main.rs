fa8509c9e3d0451632e6f46429c45b29e0d3148f
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2021-08-31 13:30:38 -0400

    Implemented the base for slash commands.

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,94 @@
-use std::env;
+use std::{env, time::Duration};
 
 use poise::{
-	async_trait,
+	command,
+	defaults::{on_error, register_slash_commands},
+	say_reply,
+	serenity,
 	serenity::{
-		client::{parse_token, Context, EventHandler},
-		model::{channel::Message, gateway::Ready},
+		client::parse_token,
+		http::Http,
+		model::{
+			gateway::Ready,
+			id::{ApplicationId, UserId},
+		},
 		Client,
 	},
+	BoxFuture,
+	Event,
+	Framework,
 };
 
-struct Handler;
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type PoiseContext<'a> = poise::Context<'a, Data, Error>;
+type PrefixContext<'a> = poise::PrefixContext<'a, Data, Error>;
+type SerenityContext = serenity::client::Context;
+
+struct Data {
+	owner_id: UserId,
+}
 
 const TOKEN_NAME: &str = "DISCORD_TOKEN";
 
-#[async_trait]
-impl EventHandler for Handler {
-	async fn message(&self, ctx: Context, msg: Message) {
-		println!("Received message: {}", msg.content);
-		if msg.content == "!ping" {
-			if let Err(e) = msg.channel_id.say(&ctx.http, "Pong").await {
-				eprintln!("Error sending message: {:?}", e);
-			}
-		}
-	}
-
-	async fn ready(&self, ctx: Context, ready: Ready) {
-		println!("{} is connected!", ready.user.name);
-		if ready.guilds.is_empty() {
-			println!("No connected guilds.");
-			return;
-		}
-		println!("Connected guilds:");
-		for guild in ready.guilds {
-			let guild_data = guild
-				.id()
-				.to_partial_guild(&ctx.http)
-				.await
-				.expect(format!("Unable to get guild with id {}", guild.id()).as_str());
-			println!(
-				"{} - {} ({})",
-				guild.id().0,
-				guild_data.name,
-				guild_data.approximate_member_count.unwrap_or(0)
-			);
-		}
+/// Event Handler
+fn listener<'a, U, E: Send>(
+	ctx: &'a SerenityContext,
+	event: &'a Event<'a>,
+	_framework: &'a Framework<U, E>,
+	_data: &'a U,
+) -> BoxFuture<'a, Result<(), E>> {
+	match event {
+		Event::Ready { data_about_bot } => Box::pin(async move {
+			ready(ctx, data_about_bot).await;
+			Ok(())
+		}),
+		_ => Box::pin(std::future::ready(Ok(()))),
 	}
 }
 
+/// Startup Function
+async fn ready(ctx: &SerenityContext, ready: &Ready) {
+	println!("{} is connected!", ready.user.name);
+	if ready.guilds.is_empty() {
+		println!("No connected guilds.");
+		return;
+	}
+	println!("Connected guilds:");
+	for guild in &ready.guilds {
+		let guild_data = guild
+			.id()
+			.to_partial_guild(&ctx.http)
+			.await
+			.expect(format!("Unable to get guild with id {}", guild.id()).as_str());
+		println!(
+			"{} - {} ({})",
+			guild.id().0,
+			guild_data.name,
+			guild_data.approximate_member_count.unwrap_or(0)
+		);
+	}
+}
+
+async fn is_owner(ctx: PrefixContext<'_>) -> Result<bool, Error> {
+	Ok(ctx.msg.author.id == ctx.data.owner_id)
+}
+
+/// Register
+#[command(check = "is_owner", hide_in_help)]
+async fn register(ctx: PrefixContext<'_>, #[flag] global: bool) -> Result<(), Error> {
+	register_slash_commands(ctx, global).await?;
+	Ok(())
+}
+
+/// Ping
+#[command(slash_command)]
+async fn ping(ctx: PoiseContext<'_>) -> Result<(), Error> {
+	say_reply(ctx, "Pong".to_owned()).await?;
+	Ok(())
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Error> {
 	let token = env::var(TOKEN_NAME).expect(
 		format!(
 			"Expected the discord token in the environment variable {}",
@@ -58,13 +98,35 @@ async fn main() {
 	);
 	let app_id = parse_token(&token).expect("Token is invalid").bot_user_id;
 
-	let mut client = Client::builder(&token)
-		.application_id(app_id.0)
-		.event_handler(Handler)
-		.await
-		.expect("Error creating the client");
+	let http = Http::new_with_token(&token);
+	let owner_id = http.get_current_application_info().await?.owner.id;
 
-	if let Err(e) = client.start().await {
+	println!("Application ID: {}", app_id);
+	println!("Owner ID: {}", owner_id);
+
+	let mut options = poise::FrameworkOptions {
+		prefix_options: poise::PrefixFrameworkOptions {
+			edit_tracker: Some(poise::EditTracker::for_timespan(Duration::from_secs(3600))),
+			..Default::default()
+		},
+		on_error: |error, ctx| Box::pin(on_error(error, ctx)),
+		listener,
+		..Default::default()
+	};
+
+	options.command(register(), |f| f);
+	options.command(ping(), |f| f);
+
+	let framework = Framework::new(
+		"-".to_owned(),
+		ApplicationId(app_id.0),
+		move |_ctx, _ready, _framework| Box::pin(async move { Ok(Data { owner_id }) }),
+		options,
+	);
+
+	if let Err(e) = framework.start(Client::builder(&token)).await {
 		eprintln!("Client error: {:?}", e);
 	}
+
+	Ok(())
 }
