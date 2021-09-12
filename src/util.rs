c6729b126eba473f25440b123899429cffd423de
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2021-09-12 17:17:37 -0400

    Made the bot escape Markdown in track titles.

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,7 @@
 use anyhow::{Context, Error};
+use lazy_static::lazy_static;
 use poise::{send_reply, serenity::builder::CreateEmbed};
+use regex::Regex;
 
 use crate::{constants::MAIN_COLOUR, PoiseContext};
 
@@ -24,11 +26,25 @@ pub async fn reply_embed(
 		.with_context(|| "Failed to send message")
 }
 
+/// Escapes a string for use in Discord, escaping all Markdown characters.
+///
+/// Square brackets can't be escaped with slashes for some reason, so they're
+/// replaced with similar-looking characters.
+pub fn escape_str(s: &str) -> String {
+	lazy_static! {
+		static ref ESCAPE_REGEX: Regex = Regex::new(r"([\\_*~`|])").unwrap();
+	}
+	ESCAPE_REGEX
+		.replace_all(s, r"\$0")
+		.replace('[', "⁅")
+		.replace(']', "⁆")
+}
+
 pub fn push_chopped_str(base: &mut String, new_str: &str, max_len: usize) {
 	const ELLIPSIS: char = '…';
 
 	if new_str.len() > max_len {
-		base.push_str(&new_str[0..(max_len - 1)]);
+		base.push_str(escape_str(&new_str[0..(max_len - 1)]).as_str());
 		base.push(ELLIPSIS);
 	} else {
 		base.push_str(new_str);
