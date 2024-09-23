645ad2b05c8d6e3bd89abe73f82979b125d18b11
Zacchary Dempsey-Plante <zacc@ztdp.ca>
2024-09-22 20:59:54 -0400

    Add support for multiple architectures to the Nix flake.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,10 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -13,68 +17,77 @@
   outputs = {
     self,
     nixpkgs,
+    flake-parts,
     rust-overlay,
     crane,
-  }: let
-    system = "x86_64-linux";
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
 
-    pkgs = import nixpkgs {
-      inherit system;
-      overlays = [rust-overlay.overlays.default];
-    };
+      perSystem = {system, ...}: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [rust-overlay.overlays.default];
+        };
 
-    rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
 
-    craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
-    src = self;
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+        src = self;
 
-    nativeBuildInputs = with pkgs; [
-      cmake
-      pkg-config
-    ];
-    buildInputs = with pkgs; [
-      libopus
-      openssl
-      sqlite
-    ];
+        nativeBuildInputs = with pkgs; [
+          cmake
+          pkg-config
+        ];
+        buildInputs = with pkgs; [
+          libopus
+          openssl
+          sqlite
+        ];
 
-    commonArgs = {
-      inherit src nativeBuildInputs buildInputs;
-      strictDeps = true;
-    };
+        commonArgs = {
+          inherit src nativeBuildInputs buildInputs;
+          strictDeps = true;
+        };
 
-    cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
-    crate = craneLib.buildPackage (commonArgs // {inherit cargoArtifacts;});
+        crate = craneLib.buildPackage (commonArgs // {inherit cargoArtifacts;});
 
-    crate-clippy = craneLib.cargoClippy (
-      commonArgs
-      // {
-        inherit cargoArtifacts;
-        cargoClippyExtraArgs = "-- --deny warnings --allow unused";
-      }
-    );
+        crate-clippy = craneLib.cargoClippy (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+            cargoClippyExtraArgs = "-- --deny warnings --allow unused";
+          }
+        );
 
-    crate-fmt-check = craneLib.cargoFmt {inherit src;};
-  in {
-    packages.${system}.default = crate;
-    checks.${system} = {
-      inherit crate crate-clippy crate-fmt-check;
-    };
-    devShells.${system}.default = pkgs.mkShell {
-      inherit buildInputs;
+        crate-fmt-check = craneLib.cargoFmt {inherit src;};
+      in {
+        packages.default = crate;
 
-      nativeBuildInputs = nativeBuildInputs ++ [rustToolchain];
+        checks = {
+          inherit crate crate-clippy crate-fmt-check;
+        };
 
-      env = {
-        RUST_BACKTRACE = "full";
+        devShells.default = pkgs.mkShell {
+          inherit buildInputs;
+
+          nativeBuildInputs = nativeBuildInputs ++ [rustToolchain];
+
+          env = {
+            RUST_BACKTRACE = "full";
+          };
+
+          shellHook = ''
+            # Required for use by RustRover, since it doesn't find the toolchain or stdlib by using the PATH
+            # RustRover must then be configured to look inside this symlink for the toolchain
+            ln --symbolic --force --no-dereference --verbose "${rustToolchain}" "./.direnv/rust-toolchain"
+          '';
+        };
       };
-
-      shellHook = ''
-        # Required for use by RustRover, since it doesn't find the toolchain or stdlib by using the PATH
-        # RustRover must then be configured to look inside this symlink for the toolchain
-        ln --symbolic --force --no-dereference --verbose "${rustToolchain}" "./.direnv/rust-toolchain"
-      '';
     };
-  };
 }
